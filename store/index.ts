fffaa080f81484981beefed28ea93d27856e1f73
skylogin <skylogin@gmail.com>
2021-06-03 13:45:00 +0900

    숙소등록 1단계 페이지/리덕스스토어 추가

--- store/index.ts
+++ store/index.ts
@@ -1,15 +1,20 @@
 import { HYDRATE, createWrapper, MakeStore } from "next-redux-wrapper";
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import { TypedUseSelectorHook, useSelector as useReduxSelector } from "react-redux";
+import {
+  TypedUseSelectorHook,
+  useSelector as useReduxSelector,
+} from "react-redux";
 
 import user from "./user";
 import common from "./common";
 import auth from "./auth";
+import registerRoom from "./registerRoom";
 
 const rootReducer = combineReducers({
-    common: common.reducer,
-    user: user.reducer,
-    auth: auth.reducer,
+  common: common.reducer,
+  user: user.reducer,
+  auth: auth.reducer,
+  registerRoom: registerRoom.reducer,
 });
 
 // 스토어 타입
@@ -18,26 +23,27 @@ export type RootState = ReturnType<typeof rootReducer>;
 let initialRootState: RootState;
 
 const reducer = (state: any, action: any) => {
-    if(action.type === HYDRATE){
-        if(state === initialRootState){
-            return {
-                ...state, ...action.payload
-            };
-        }
+  if (action.type === HYDRATE) {
+    if (state === initialRootState) {
+      return {
+        ...state,
+        ...action.payload,
+      };
     }
-    return rootReducer(state, action);
+  }
+  return rootReducer(state, action);
 };
 
 // 타입이 지원되는 커스텀 useSelector
 export const useSelector: TypedUseSelectorHook<RootState> = useReduxSelector;
 
 const initStore: MakeStore = () => {
-    const store = configureStore({
-        reducer,
-        devTools: true,
-    });
-    initialRootState = store.getState();
-    return store;
+  const store = configureStore({
+    reducer,
+    devTools: true,
+  });
+  initialRootState = store.getState();
+  return store;
 };
 
 export const wrapper = createWrapper(initStore);
