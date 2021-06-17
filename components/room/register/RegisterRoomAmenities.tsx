d9f17eed3f7af2959dc85d721f8e5977a03ea96d
skylogin <skylogin@gmail.com>
2021-06-17 23:22:13 +0900

    어메니티 페이지 완료

--- components/room/register/RegisterRoomAmenities.tsx
+++ components/room/register/RegisterRoomAmenities.tsx
@@ -4,12 +4,15 @@ import { useDispatch } from "react-redux";
 import styled from "styled-components";
 import palette from "../../../styles/palette";
 
-// import { useSelector } from "../../../store";
-// import { registerRoomActions } from "../../../store/registerRoom";
+import { useSelector } from "../../../store";
+import { registerRoomActions } from "../../../store/registerRoom";
 
+import CheckboxGroup from "../../common/CheckboxGroup";
 // import Button from "../../common/Button";
 // import Selector from "../../common/Selector";
 // import Input from "../../common/Input";
+
+import { amentityList } from "../../../lib/staticData";
 
 import RegisterRoomFooter from "./RegisterRoomFooter";
 
@@ -36,6 +39,14 @@ const Container = styled.div`
 `;
 
 const RegisterRoomAmenities: React.FC = () => {
+  const dispatch = useDispatch();
+
+  const amenities = useSelector((state) => state.registerRoom.amenities);
+
+  const onChangeAmenities = (selected: string[]) => {
+    dispatch(registerRoomActions.setAmenities(selected));
+  }
+
   return (
     <Container>
       <h2>어떤 편의 시설을 제공하시나요?</h2>
@@ -44,9 +55,17 @@ const RegisterRoomAmenities: React.FC = () => {
         일반적으로 게스트가 기대하는 편의 시설 목록입니다. 숙소를 등록한 후 언제든 편의시설을 추가할 수 있어요.
       </p>
 
+      <div className="register-room-amenites-checkbox-group-wrapper">
+        <CheckboxGroup
+          value={amenities}
+          onChange={onChangeAmenities}
+          options={amentityList}
+        />
+      </div>
+
       <RegisterRoomFooter
-        prevHref="/room/register/geometry"
-        nextHref="/room/register/"
+        prevHref="/room/register/location"
+        nextHref="/room/register/conveniences"
       />
     </Container>
   );
