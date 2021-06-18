e47ff54b27313fa74e22b680ac51a47483642e34
skylogin <skylogin@gmail.com>
2021-06-18 12:53:58 +0900

    날짜선택페이지 완료

--- components/room/register/RegisterRoomDate.tsx
+++ components/room/register/RegisterRoomDate.tsx
@@ -24,32 +24,93 @@ const Container = styled.div`
     color: ${palette.gray_76};
     margin-bottom: 6px;
   }
-  
+  .register-room-date-wrapper {
+    display: flex;
+    align-items: center;
+    label {
+      span {
+        display: block;
+        margin-bottom: 8px;
+      }
+    }
+    input {
+      display: block;
+      position: relative;
+      width: 100%;
+      height: 46px;
+      padding: 0 11px;
+      border: 1px solid ${palette.gray_eb};
+      border-radius: 4px;
+      font-size: 16px;
+      outline: none;
+      & ::placeholder {
+        color: ${palette.gray_76};
+      }
+      & :focus {
+        border-color: ${palette.dark_cyan};
+      }
+    }
+    .register-room-start-date {
+      margin-right: 20px;
+    }
+  }
 `;
 
 const RegisterRoomDate: React.FC = () => {
   const dispatch = useDispatch();
 
   const startDate = useSelector((state) => state.registerRoom.startDate);
+  const endDate = useSelector((state) => state.registerRoom.endDate);
+
+  const dateStartDate = startDate ? new Date(startDate): null;
+  const dateEndDate = endDate ? new Date(endDate): null;
 
   const onChangeStartDate= (date: Date | null) => {
-    console.log(date);
     dispatch(registerRoomActions.setStartDate(date? date.toISOString(): null));
+  }
+  const onChangeEndDate= (date: Date | null) => {
+    dispatch(registerRoomActions.setEndDate(date? date.toISOString(): null));
   }
 
   return (
     <Container>
       <h2>예약 가능 여부 설정.</h2>
       <h3>11단계</h3>
+      <div className="register-room-date-wrapper">
+        <div className="register-room-start-date">
+          <label>
+            <span>예약 시작일</span>
+            <DatePicker 
+              selected={startDate? new Date(startDate): null}
+              onChange={onChangeStartDate} 
+              monthsShown={2}
+              selectsStart
+              startDate={dateStartDate}
+              endDate={dateEndDate}
+              minDate={new Date()}
+            />
+          </label>
+        </div>
 
-      <DatePicker 
-        selected={startDate? new Date(startDate): null}
-        onChange={onChangeStartDate} 
-      />
+        <div className="register-room-end-date">
+          <label>
+            <span>예약 마감일</span>
+            <DatePicker 
+              selected={endDate? new Date(endDate): null}
+              onChange={onChangeEndDate} 
+              monthsShown={2}
+              selectsStart
+              startDate={dateStartDate}
+              endDate={dateEndDate}
+              minDate={dateStartDate}
+            />
+          </label>
+        </div>
+      </div>
 
       <RegisterRoomFooter
         prevHref="/room/register/price"
-        nextHref="/room/register/"
+        nextHref="/room/register/checklist"
       />
     </Container>
   );
