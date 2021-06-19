ff177cc3db8df33965a142c143a2edac0dd35844
skylogin <skylogin@gmail.com>
2021-06-19 11:57:51 +0900

    숙소 상세페이지 및 예약api 구성

--- lib/api/room.ts
+++ lib/api/room.ts
@@ -26,3 +26,5 @@ type GetRoomListAPIQueries = {
 export const getRoomListAPI = (queries: GetRoomListAPIQueries) => {
   return axios.get<RoomType[]>(makeQueryString("/api/rooms", queries));
 }
+
+export const getRoomAPI = (roomId: number) => axios.get<RoomType>(`/api/rooms/${roomId}`);
